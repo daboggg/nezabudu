f1ce7b16781d46942ccfefcca342575367e4e0c9
daboggg <vzinin@list.ru>
2024-01-17 17:46:42 +0300

    in_date.py add date

--- parser/in_date.py
+++ parser/in_date.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from re import Match
 
 from parser.errors import ParserException
-from parser.utils import check_hours, check_minutes_or_seconds, check_variants, check_days, check_day_of_month
+from parser.utils import check_hours, check_minutes_or_seconds, check_variants
 
 
 def in_date(remind_cmd: list[str]) -> dict[str, datetime]:
@@ -92,12 +92,10 @@ def in_date(remind_cmd: list[str]) -> dict[str, datetime]:
                     (?P<minute>\d\d)
                 |\d+)?
             |
-            (?:
             [^.:-](?P<only_time>
             \d\d[.:-]\d\d
             |
             \d\d
-            )
             )
             )
                 """, re.VERBOSE)
@@ -112,6 +110,9 @@ def in_date(remind_cmd: list[str]) -> dict[str, datetime]:
         set_date_tomorrow_or_after_tomorrow(1, result, params)
     elif result.group("date") == "послезавтра":
         set_date_tomorrow_or_after_tomorrow(2, result, params)
+    elif re.search(r"\d\d[.:-]\d\d[.:-]\d\d\d?\d?", result.group("date")):
+        day, month, year = re.split(r":|-|\.", result.group("date"))
+        set_date(int(year), int(month), int(day), result, params)
     elif result.group("date") and (found := check_variants(result.group("date"))):
         set_date_on_month_or_day_of_week(found[1], result, params, found[0])
 
@@ -133,6 +134,19 @@ def get_time(result: Match):
         hour = check_hours(int(result.group("hour")))
         minute = check_minutes_or_seconds(int(result.group("minute")))
     return [hour, minute]
+
+
+# установка даты (dd:mm:YYYY)
+def set_date(year: int, month: int, day: int, result: Match, params: dict):
+    if year < 100: year = year + 2000
+    hour, minute = get_time(result)
+    now = datetime.now()
+    date_for_checking = datetime(year=year, month=month, day=day, hour=hour, minute=minute)
+
+    if now < date_for_checking:
+        params["run_date"] = date_for_checking
+    else:
+        raise ParserException("the date is earlier than the current one")
 
 
 # установка даты (завтра, послезавтра)
@@ -175,8 +189,8 @@ def set_date_on_month_or_day_of_week(numb: int, result: Match, params: dict, per
 # in_date("послезавтра в 14".split(" "))                    ###
 # in_date("18 апреля в 14".split(" ")) #                    ###
 # in_date("16 сентября в 10-20".split(" "))                 ###
+# in_date("17.04.2024 в 9-15".split(" "))                   ###
 # in_date("в 13-30".split(" "))
-# in_date("17.04.2024 в 9-15".split(" "))
 
 
 print(in_date("17 февраля в 02.32".split(" ")))
@@ -186,15 +200,19 @@ print(in_date("d понедельник 20.36".split(" ")))
 print(in_date("d понедельник f 20.36".split(" ")))
 print(in_date("20  среду f 14.26".split(" ")))
 print(in_date("20 jd понедельник f 20".split(" ")))
-# print(in_date("20 jd 12.12.24 f 20.36".split(" ")))
-# print(in_date("20 ffjd 12.12.24 fff 23".split(" ")))
+print(in_date("20 jd 12.12.26 f 20.36".split(" ")))
+print(in_date("20 ffjd 12.12.24 fff 23".split(" ")))
+print(in_date("12.12.24 fff 07".split(" ")))
+print(in_date("d 12.12.24 df".split(" ")))
 # print(in_date("в dfffdsdf dfffdsdf dfffdsdf 20".split(" ")))
 # print(in_date("в 20".split(" ")))
+# print(in_date("в 07".split(" ")))
 # print(in_date("в 20.25".split(" ")))
 
 
 print(in_date(" завтра".split(" ")))
 print(in_date("завтра".split(" ")))
+print(in_date("завтра в 2.11".split(" ")))
 print(in_date("d завтра 20.36".split(" ")))
 print(in_date("d завтра f 20.36".split(" ")))
 print(in_date("20 jd послезавтра f 20.36".split(" ")))
